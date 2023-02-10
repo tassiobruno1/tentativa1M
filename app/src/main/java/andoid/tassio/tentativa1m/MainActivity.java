d13dd0de25d42e7b056e09ab817b95e7673b423b
Tássio Bruno <tssiobruno1@gmail.com>
2023-02-10 18:16:18 -0300

    Initial commit

---
+++ app/src/main/java/andoid/tassio/tentativa1m/MainActivity.java
@@ -0,0 +1,21 @@
+package andoid.tassio.tentativa1m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void teladois(View view) {
+        Intent in = new Intent(MainActivity.this, TelaDois.class);
+        startActivity(in);
+    }
+}
